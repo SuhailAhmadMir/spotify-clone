ab565e01446c62bcbaea8956d8bbe016f489284c
SuhailAhmadMir <mir.suhail907@yahoo.com>
2020-08-29 15:29:31 +0530

    context API setup

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
     const hash = getTokenFromUrl();
-    console.log("TOKKEN >>>>", hash);
     const _token = hash.access_token;
-    console.log(_token);
+    window.location.hash = "";
 
     if (_token) {
-      return setToken(_token);
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
+    console.log("SPOTIFY", spotify);
+    console.log("I HAVE A TOKKEN ", _token);
   }, []);
-  return (
-    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
-  );
+  console.log("USER>>>", user);
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
